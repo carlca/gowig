a2f1fb0d148823c541157036529bdd9c20a9e344
carlca <carl.caulkett@gmail.com>
2021-05-31 22:48:17 +0100

    added cmd and output

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"github.com/carlca/goprefix/output"
+)
+
+func main() {
+	filename := os.Args[1]
+	data, err := ioutil.ReadFile(filename)
+    if err != nil {
+		fmt.Println("File reading error", err)
+        return
+    }
+	fmt.Println(len(data))
+	if err != nil {
+		fmt.Println("File printing error", err)
+	}
+
+	p := output.Param{"device", "Chain"}
+	streamPos := 0x
+	fmt.Println(p.Key)
+	fmt.Println(p.Value)
+}
+
+func GetChunk(streamPos int64, data []byte) int64 {
+	
+}
---
+++ output/output.go
@@ -0,0 +1,6 @@
+package output
+
+type Param struct {
+	Key string
+	Value string
+}
