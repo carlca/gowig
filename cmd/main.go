3ed1ba1a3fecb39397f9d4729d45a66ffef8aa4b
carlca <carl.caulkett@gmail.com>
2021-06-01 00:36:34 +0100

    tried to get delve working

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
-	"io/ioutil"
 	"os"
+
 	"github.com/carlca/gowig/output"
 )
 
 func main() {
 	filename := os.Args[1]
-	data, err := ioutil.ReadFile(filename)
-    if err != nil {
-		fmt.Println("File reading error", err)
-        return
-    }
-	fmt.Println(len(data))
+
+	f, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("File printing error", err)
+		fmt.Println("File reading error", err)
+		return
 	}
 
+	// fmt.Println(len(data))
+	// if err != nil {
+	// 	fmt.Println("File printing error", err)
+	// }
+
 	p := output.Param{"device", "Chain"}
-	// streamPos := 0x
 	fmt.Println(p.Key)
 	fmt.Println(p.Value)
+
+	streamPos := 0x3e
+	chunk, err := readFromFile(f, streamPos, 4) //read 4 bytes in the file from byte 0x3e
+
+	var size int64
+	buf := bytes.NewReader(chunk)
+	err = binary.Read(buf, binary.LittleEndian, &size)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	fmt.Println(size)
+
 }
 
-func GetChunk(streamPos int64, data []byte) int64 {
-	return 0
+func readFromFile(file *os.File, offset, size int) ([]byte, error) {
+	res := make([]byte, size)
+	if _, err := file.ReadAt(res, int64(offset)); err != nil {
+		return nil, err
+	}
+	return res, nil
 }
