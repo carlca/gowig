77528e21e39553ccdb2ac848aefb4f47d0252ef8
carlca <carl.caulkett@gmail.com>
2021-06-02 00:54:30 +0100

    getting there

--- cmd/main.go
+++ cmd/main.go
@@ -27,8 +27,17 @@ func main() {
 	p := output.Param{"device", "Chain"}
 	fmt.Println(p.Key)
 	fmt.Println(p.Value)
-	
 
+	streamPos := 0x7f
+	chunk, err := readFromFile(f, streamPos, 4) //read 4 bytes in the file from byte 0x7f
+
+	var size int32
+	buf := bytes.NewReader(chunk)
+	err = binary.Read(buf, binary.BigEndian, &size)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	fmt.Println(size)
 }
 
 func readFromFile(file *os.File, offset, size int) ([]byte, error) {
